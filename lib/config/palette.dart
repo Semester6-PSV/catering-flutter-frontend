0e87e9862ddb1ef4408702cc69984073eaab6ea1
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2022-05-08 22:42:58 +0200

    Add custom app fonts

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 const COLOR_RED = Color.fromRGBO(237, 28, 36, 1);
-const COLOR_RED_RARK = Color.fromRGBO(192, 13, 13, 1);
+const COLOR_RED_DARK = Color.fromRGBO(192, 13, 13, 1);
 const COLOR_BLACK = Color.fromRGBO(0, 0, 0, 1);
 const COLOR_GREY = Color.fromRGBO(51, 51, 51, 1);
 const COLOR_GREY_LIGHT = Color.fromRGBO(102, 102, 102, 1);
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'dart:developer' as developer;
 void main() {
   runApp(const CateringApp());
 }
+
 class CateringApp extends StatelessWidget {
   const CateringApp({Key? key}) : super(key: key);
 
@@ -17,6 +18,30 @@ class CateringApp extends StatelessWidget {
           primaryColor: COLOR_RED,
           colorScheme:
               ColorScheme.fromSwatch().copyWith(secondary: COLOR_GREY)),
+      home: TestPage(),
     );
+  }
+}
+
+class TestPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+                backgroundColor: COLOR_RED_DARK,
+                title: const Text('Test App Bar',
+                    style: TextStyle(fontFamily: 'Klavika-Medium'))),
+            body: Container(
+                width: size.width,
+                height: size.height,
+                padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
+                color: COLOR_WHITE,
+                child: Text(
+                  'PSV Catering',
+                  style: TextStyle(fontFamily: 'Klavika-Bold'),
+                ))));
   }
 }
