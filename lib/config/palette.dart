439a5b8390eedb9591ba3353609020de92995d1a
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2022-05-08 22:02:18 +0200

    Add global app colors to config

---
+++ lib/config/palette.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+const COLOR_RED = Color.fromRGBO(237, 28, 36, 1);
+const COLOR_RED_RARK = Color.fromRGBO(192, 13, 13, 1);
+const COLOR_BLACK = Color.fromRGBO(0, 0, 0, 1);
+const COLOR_GREY = Color.fromRGBO(51, 51, 51, 1);
+const COLOR_GREY_LIGHT = Color.fromRGBO(102, 102, 102, 1);
+const COLOR_WHITE = Color.fromRGBO(255, 255, 255, 1);
