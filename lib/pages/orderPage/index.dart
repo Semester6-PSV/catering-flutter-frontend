cce969723daebd29292cb751bdc98dc3532dea8d
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2022-05-22 15:48:09 +0200

    Add bottom modal sheet to inspect current ordered products

--- lib/pages/orderPage/index.dart
+++ lib/pages/orderPage/index.dart
@@ -57,8 +57,84 @@ class OrderPageState extends State<OrderPage> {
     }
   }
 
-  void openOrderPopup() {
-    return;
+  void openOrderPopup(context) {
+    List<Widget> addedProducts = <Widget>[];
+    double totalPrice = 0;
+
+    for (var product in cateringProducts) {
+      if (product.amount > 0) {
+        totalPrice += product.amount * product.price;
+
+        addedProducts.add(Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Text(product.amount.toString(),
+                    style: const TextStyle(
+                        fontFamily: 'Klavika-Medium', fontSize: 20)),
+                const SizedBox(width: 10),
+                Text(product.title,
+                    style: const TextStyle(
+                        fontFamily: 'Klavika-Light', fontSize: 20))
+              ],
+            ),
+            Expanded(
+                child: Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                  '€' + productPriceToString(product.amount * product.price),
+                  style: const TextStyle(
+                      fontFamily: 'Klavika-Medium', fontSize: 20)),
+            )),
+            const SizedBox(height: 30)
+          ],
+        ));
+      }
+    }
+
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(30),
+            decoration: const BoxDecoration(
+              color: COLOR_WHITE,
+            ),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              const Text('Je bestelling',
+                  style: TextStyle(fontFamily: 'Klavika-Medium', fontSize: 28)),
+              const SizedBox(height: 20),
+              const Text('Bestelde producten',
+                  style: TextStyle(fontFamily: 'Klavika-Light', fontSize: 20)),
+              const SizedBox(height: 10),
+              SizedBox(
+                  width: double.infinity,
+                  child: Column(children: addedProducts)),
+              const SizedBox(height: 30),
+              Container(
+                  width: double.infinity,
+                  height: 2,
+                  color: const Color.fromRGBO(219, 219, 219, 1)),
+              const SizedBox(height: 20),
+              Align(
+                  alignment: Alignment.centerRight,
+                  child: Text('Totaal €' + totalPrice.toString(),
+                      style: const TextStyle(
+                          fontFamily: 'Klavika-Medium', fontSize: 20))),
+              const SizedBox(height: 20),
+              Row(children: [
+                Expanded(
+                    child: CustomButton(
+                        text: 'Bestelling afronden',
+                        type: ButtonType.primary,
+                        onClick: routeToCheckoutPage))
+              ])
+            ]),
+          );
+        });
   }
 
   void routeToCheckoutPage() {
@@ -157,7 +233,9 @@ class OrderPageState extends State<OrderPage> {
                       child: CustomButton(
                     text: 'Bekijk bestelling',
                     type: ButtonType.secondary,
-                    onClick: openOrderPopup,
+                    onClick: () {
+                      openOrderPopup(context);
+                    },
                   )),
                   VerticalDivider(
                     width: gridPadding,
